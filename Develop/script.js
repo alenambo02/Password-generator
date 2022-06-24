8268a0420f97e9e3bbf54d0d5a054cc201ca0318
Alejandra Izquierdo <allleizq@gmail.com>
2022-06-24 06:48:30 -0700

    added variables

--- Develop/script.js
+++ Develop/script.js
@@ -1,9 +1,9 @@
 // Assignment code here
 
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
-
+var specialCharacters = "*&^%$#@!"
+var userlength = 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -15,3 +15,15 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// I added a prompt for window above page to appear with password length
+var generatePassword = function () {
+var userlength = prompt("Please enter amount of characters wanted in password below");
+  if (!userlength) {
+    return;
+  }
+var lowerCase = confirm("Do you want lowercase letters in your password?")
+var upperCase = confirm("Do you want uppercase letters in your password?")
+var numeric = confirm("Do you want numbers in your password?")
+var specialCharacters = confirm("Do you want special characters in your password?")
+}
